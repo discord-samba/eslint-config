3cb92ada2b9f02768d6f74e01b32e376db32d9a9
Zack Campbell <zajrik@gmail.com>
2019-10-17 10:13:32 -0500

    Fix config loader, add dependencies
    Not sure why but it complains about eslint and typescript not existing, despite
    definitely being installed in my project. IDK. Maybe it's because it's currently
    linked and not actually installed from github/npm

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./.eslintrc');
+const FS = require('fs');
+const Path = require('path');
+
+// Load .eslintrc and remove comments
+const config = FS.readFileSync(Path.join(__dirname, '.eslintrc'))
+	.toString()
+	.split('\n')
+	.map(l => l.replace(/^\s*\/\/.+$/gm, ''))
+	.join('\n');
+
+module.exports = JSON.parse(config);
